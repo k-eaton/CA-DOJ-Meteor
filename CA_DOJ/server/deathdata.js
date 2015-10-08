968c95fa558e4b39f3d684e16b3bcf7af1621617
Katrina Eaton <katrina.eaton@gmail.com>
2015-10-08 10:20:35 -0700

    add directions to add data to mongo

--- CA_DOJ/server/deathdata.js
+++ CA_DOJ/server/deathdata.js
@@ -1,5 +1,13 @@
-// Saved for "posterity" I spent 4 fucking hours on it,
-// I'm not trashing it now, even if it didn't load the file correctly.
+/* Saved for "posterity" I spent 4 fucking hours on it,
+/  I'm not trashing it now, even if it didn't load the file correctly.
+/
+/  PS: This will load the file correctly from the command line:
+/
+/  mongoimport -h localhost:3001 --db meteor --collection deathdata --type csv --file duplicate.csv --headerline
+/
+/  Yes, localhost:3001 is correct.
+*/
+
 
 // if (Deathdata.find().count() === 0) {
 // 	var filename = 'death.csv';
